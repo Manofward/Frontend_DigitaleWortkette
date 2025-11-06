b37ba406278a4a442d5e5634d5832c3075031753
MeyerJan <janikmeyer1607@outlook.de>
2025-11-06 14:46:59 +0100

    file for Widgets that can be used more times with less code

---
+++ lib/Widgets/custom_scaffold.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class ButtonCentered extends StatelessWidget {
+  final String label;
+  final IconData icon;
+  final VoidCallback onPressed;
+
+  const ButtonCentered({
+    super.key,
+    required this.label,
+    required this.icon,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: MediaQuery.of(context).size.width * 0.8,
+      height: 60,
+      child: ElevatedButton.icon(
+        onPressed: onPressed,
+        icon: Icon(icon, size: 28),
+        label: Text(
+          label,
+          style: const TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
