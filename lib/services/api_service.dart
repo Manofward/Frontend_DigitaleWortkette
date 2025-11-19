665d6a98a29a61b0c495992b2cae92adc6bed9ae
MeyerJan <janikmeyer1607@outlook.de>
2025-11-19 11:13:33 +0100

    adding the new ip for testing

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -3,7 +3,8 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  static const String baseUrl = 'http://172.16.34.142:5000/api/v1/dwk';
+  static const String baseUrl = 'http://172.16.34.76:5000/api/v1/dwk';
+  //static const String baseUrl = 'http://10.0.0.2:5000/api/v1/dwk';
 
   // --------------------------
   // Basic GET
